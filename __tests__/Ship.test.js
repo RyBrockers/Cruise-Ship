ee677f041c442fe08caee60d4927597944cd7166
Ryan Brockley <rybrockers@yahoo.com>
2018-09-23 22:34:10 +0100

    Added dock method and ship tests

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -1,4 +1,7 @@
+/* globals describe it expect */
 const Ship = require('../src/Ship.js');
+const Port = require('../src/Port.js');
+
 
 
 describe('Ship', () => {
@@ -6,15 +9,27 @@ describe('Ship', () => {
         expect(new Ship()).toBeInstanceOf(Object);
     })
     it('Has a starting port', () => {
-        const ship = new Ship('Hull');
-        expect(ship.startingPort).toBe('Hull');
+        const port = new Port("Hull")
+        const ship = new Ship(port);
+        expect(ship.currentPort).toBe(port);
     })
 })
 
 describe('setSail', () => {
     it('Can sail from a port', () => {
-        const ship = new Ship('Hull');
+        const port = new Port('Hull')
+        const ship = new Ship(port);
         ship.setSail();
-        expect(ship.startingPort).toBeFalsy();
+        expect(ship.currentPort).toBeFalsy();
+    })
+})
+
+describe('dock', () => {
+    it('Can dock at different ports', () => {
+        const liverpool = new Port('Liverpool');
+        const ship = new Ship(liverpool);
+        const calais = new Port('Calais');
+        ship.dock(calais);
+        expect(ship.currentPort).toBe(calais)
     })
 })
--- src/Ship.js
+++ src/Ship.js
@@ -1,12 +1,14 @@
-const Ship = function () {
-    this.startingPort = 'Hull';
+const Ship = function (port) {
+    this.currentPort = port;
 }
 
 
 Ship.prototype.setSail = function setSail() {
-    this.startingPort = null;
+    this.currentPort = null;
 }
-
+Ship.prototype.dock = function dock(port) {
+    this.currentPort = port;
+}
 
 
 
