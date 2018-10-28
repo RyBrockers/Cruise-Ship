ff9595830dae3972fcea6e5a60c8e602772f0fc4
Ryan Brockley <rybrockers@yahoo.com>
2018-10-28 17:07:23 +0000

    UI coming along

--- src/controller.js
+++ src/controller.js
@@ -31,20 +31,56 @@
       portsElement.style.width = `${portsElementWidth + 256}px`;
     });
   };
-  Controller.prototype.renderShip = function renderShip(ship) {
+  Controller.prototype.renderShip = function renderShip() {
     const ship = this.ship;
     const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
     const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
     const shipElement = document.querySelector('#ship');
-    shipElement.style.top = `${portElement.offsetTop}px`;
-    shipElement.style.left = `${portElement.offsetLeft}px`;
     shipElement.style.top = `${portElement.offsetTop + 32}px`;
     shipElement.style.left = `${portElement.offsetLeft - 32}px`;
+  };
+  Controller.prototype.setSail = function setSail() {
+    const ship = this.ship;
+
+    const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+    const nextPortIndex = currentPortIndex + 1;
+    const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}']`);
+    if (!nextPortElement) {
+      this.renderMessage(`End of the line ${ship.currentPort.name}`);
+    } else {
+      this.renderMessage(`Now departing ${ship.currentPort.name}`);
+    }
+
+    ship.setSail();
+
+    const shipElement = document.querySelector('#ship');
+    const sailInterval = setInterval(() => {
+      const shipLeft = parseInt(shipElement.style.left, 10);
+      if (shipLeft === (nextPortElement.offsetLeft - 32)) {
+        ship.dock();
+        this.renderMessage(`Arrived at ${ship.currentPort.name}`);
+        clearInterval(sailInterval);
+      }
+
+      shipElement.style.left = `${shipLeft + 1}px`;
+    }, 20);
+  };
+
+  Controller.prototype.renderMessage = function renderMessage(message) {
+    const messageElement = document.createElement('div');
+    messageElement.id = 'message';
+    messageElement.innerHTML = message;
+
+    const viewport = document.querySelector('#viewport');
+    viewport.appendChild(messageElement);
+    setTimeout(() => {
+      viewport.removeChild(messageElement);
+    }, 2000);
   };
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Controller;
   } else {
     window.Controller = Controller;
-  };
+  }
 }());
